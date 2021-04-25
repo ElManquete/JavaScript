f413ef159bf696c72d99cbf8e7120f7ffe5e472d
Dorian <dorian.garcia.guzman@gmail.com>
2021-04-25 16:11:31 +0200

    Terminado ejercicio5

---
+++ ejercicio5.js
@@ -0,0 +1,11 @@
+const CONSTANTE = 1;
+function helloWorld () {
+  let miVar = 2;
+  console.log (CONSTANTE);
+  console.log (miVar);
+  return;
+}
+
+console.log (CONSTANTE);
+helloWorld();
+console.log (miVar);
